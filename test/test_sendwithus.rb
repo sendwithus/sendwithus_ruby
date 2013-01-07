d7903261e80833bf61bbd69343588147882a1bd0
Matt Harris <harrismc@gmail.com>
2013-01-07 15:51:21 -0800

    simple test ...

--- test/test_sendwithus.rb
+++ test/test_sendwithus.rb
@@ -1,4 +1,12 @@
 
+require 'test/unit'
+require 'lib/sendwithus'
+
+API_PORT = '8000'
+API_HOST = 'localhost'
+API_PROTO = 'http'
+API_KEY = ''
+
 class TestRubygemSendwithus < Test::Unit::TestCase
 
     def setup
@@ -6,6 +14,15 @@ class TestRubygemSendwithus < Test::Unit::TestCase
 
     def test_truth
         assert true
+    end
+
+    def test_simple
+        api = SendWithUs::API.new(API_KEY, :api_host => API_PORT, :api_port => API_PORT, :api_proto => API_PROTO)
+
+        data = {'name' => 'Jimmy'}
+        api.send('test_email', 'matt@sendwithus.com', data)
+
+        return true
     end
 end
 
