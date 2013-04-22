ff305767e7a3db7d0a4d937ace7c345af586c4fe
forkata <chris.todorov@gmail.com>
2013-04-22 02:21:45 +0000

    Updated client to v1_0 of api and added emails endpoint.

--- lib/send_with_us/api.rb
+++ lib/send_with_us/api.rb
@@ -20,9 +20,17 @@ module SendWithUs
       @configuration = SendWithUs::Config.new(settings)
     end
 
-    def send_with(email_id, to, data = {})
-      payload = { email_id: email_id, email_to: to, email_data: data }.to_json
+    def send_with(email_id, to, data = {}, from = {})
+      if from.any?
+        payload = { email_id: email_id, recipient: to, sender: from, email_data: data }.to_json
+      else
+        payload = { email_id: email_id, recipient: to, email_data: data }.to_json
+      end
       SendWithUs::ApiRequest.new(@configuration).send_with(payload)
+    end
+
+    def emails()
+      SendWithUs::ApiRequest.new(@configuration).get(:emails)
     end
 
   end
