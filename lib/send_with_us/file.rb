c36cdc2dd41eb88a8c5274fab2a568ffceadd755
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2018-02-22 11:18:23 +0100

    Drop SimpleDelegator dependency to avoid bad performance ruby@1.8

--- lib/send_with_us/file.rb
+++ lib/send_with_us/file.rb
@@ -1,16 +1,21 @@
 module SendWithUs
-  class File < SimpleDelegator
+  class File
+    attr_accessor :attachment
+
     def initialize(file_data, opts = {})
-      attachment = if file_data.is_a?(String)
-                     SendWithUs::Attachment.new(file_data)
-                   else
-                     if file_data[:data] and file_data[:id]
-                       file_data
-                     else
-                       SendWithUs::Attachment.new(file_data[:attachment], file_data[:filename])
-                     end
-                   end
-      super(attachment)
+      @attachment = if file_data.is_a?(String)
+                      SendWithUs::Attachment.new(file_data)
+                    else
+                      if file_data[:data] and file_data[:id]
+                        file_data
+                      else
+                        SendWithUs::Attachment.new(file_data[:attachment], file_data[:filename])
+                      end
+                    end
+    end
+
+    def to_h
+      attachment.to_h
     end
   end
 end
